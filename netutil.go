3eaa67a3b8300d48ee1724f5d178805ed706da5e
vbox-sk <vbox@securitykiss.com>
2017-07-25 18:34:51 +0100

    ThisHostPublicIPs

--- netutil.go
+++ netutil.go
@@ -1,34 +1,84 @@
 package netutil
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 )
 
+//ipRange - a structure that holds the start and end of a range of ip addresses
+type ipRange struct {
+	start net.IP
+	end   net.IP
+}
+
+// inRange - check to see if a given ip address is within a range given
+func inRange(r ipRange, ipAddress net.IP) bool {
+	// strcmp type byte comparison
+	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+		return true
+	}
+	return false
+}
+
+var privateRanges = []ipRange{
+	ipRange{
+		start: net.ParseIP("10.0.0.0"),
+		end:   net.ParseIP("10.255.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("100.64.0.0"),
+		end:   net.ParseIP("100.127.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("172.16.0.0"),
+		end:   net.ParseIP("172.31.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.0.0.0"),
+		end:   net.ParseIP("192.0.0.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.168.0.0"),
+		end:   net.ParseIP("192.168.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("198.18.0.0"),
+		end:   net.ParseIP("198.19.255.255"),
+	},
+}
+
+// isPrivateSubnet - check to see if this ip is in a private subnet
+func isPrivateSubnet(ipAddress net.IP) bool {
+	// my use case is only concerned with ipv4 atm
+	if ipCheck := ipAddress.To4(); ipCheck != nil {
+		// iterate over all our ranges
+		for _, r := range privateRanges {
+			// check if this ip is in a private range
+			if inRange(r, ipAddress) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func Example() {
-	ips := FindHostIPs()
+	ips := ThisHostPublicIPs()
 	fmt.Printf("ipv4=%v\n", ips)
 
 }
 
-func FindHostIPs() []string {
-	return FindIPs([]string{"eth0", "wlan0", "eth1", "em0", "em1"})
-}
-
-// return IPv4 addresses of given interfaces
-func FindIPs(ifnames []string) (ips []string) {
-	for _, ifname := range ifnames {
-		iface, err := net.InterfaceByName(ifname)
-		if err != nil {
-			continue
-		}
-		if addrs, err := iface.Addrs(); err == nil {
-			for _, addr := range addrs {
-				if ipnet, ok := addr.(*net.IPNet); ok {
-					if ipnet.IP.To4() != nil {
-						ips = append(ips, ipnet.IP.To4().String())
-					}
-				}
+func ThisHostPublicIPs() (ips []string) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil
+	}
+	for _, a := range addrs {
+		switch addr := a.(type) {
+		case *net.IPNet:
+			if addr.IP.IsGlobalUnicast() && !isPrivateSubnet(addr.IP) {
+				ips = append(ips, addr.IP.String())
 			}
 		}
 	}
